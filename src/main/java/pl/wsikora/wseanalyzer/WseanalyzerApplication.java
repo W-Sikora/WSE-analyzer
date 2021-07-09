680f8b8ae59b6d51285055d777b7ccb248246e4b
W-Sikora <242612@student.pwr.edu.pl>
2021-07-09 21:49:59 +0200

    added and enabled swagger

--- src/main/java/pl/wsikora/wseanalyzer/WseanalyzerApplication.java
+++ src/main/java/pl/wsikora/wseanalyzer/WseanalyzerApplication.java
@@ -2,8 +2,10 @@ package pl.wsikora.wseanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class WseanalyzerApplication {
 
     public static void main(String[] args) {
